326984fa31b30f0e598fc04ecd0fa3118c71bedb
Ayan Mujahid <156477951+ayanmujahid@users.noreply.github.com>
2026-02-05 13:57:20 -0800

    Update script.php

--- include/script.php
+++ include/script.php
@@ -1,13 +1,13 @@
-    <script src="../cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="../cdn.jsdelivr.net/npm/bootstrap%405.0.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../cdn.jsdelivr.net/gh/michalsnik/aos%401.0.1/dist/aos.js"></script>
-    <script src="../cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
-    <script src="../cdn.jsdelivr.net/npm/swiper%409/swiper-bundle.min.js"></script>
-    <script src="../cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <script src="../cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-    <script src="../cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
-    <script src="../unpkg.com/gsap%403.14.2/dist/ScrollSmoother.min.js"></script>
-    <script src="assets/js/custom.js"></script>
+    <script src="../cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer></script>
+    <script src="../cdn.jsdelivr.net/npm/bootstrap%405.0.1/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="../cdn.jsdelivr.net/gh/michalsnik/aos%401.0.1/dist/aos.js" defer></script>
+    <script src="../cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" defer></script>
+    <script src="../cdn.jsdelivr.net/npm/swiper%409/swiper-bundle.min.js" defer></script>
+    <script src="../cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" defer></script>
+    <script src="../cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" defer></script>
+    <script src="../cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" defer></script>
+    <script src="../unpkg.com/gsap%403.14.2/dist/ScrollSmoother.min.js" defer></script>
+    <script src="assets/js/custom.js" defer></script>
     <!-- Start of ultradigitalsolution Zendesk Widget script -->
     <!-- <script id="ze-snippet" src="../static.zdassets.com/ekr/snippet82a7.js?key=cb0f074a-fed2-429d-953d-ab53aee123aa"> </script> -->
     <!-- End of ultradigitalsolution Zendesk Widget script -->
